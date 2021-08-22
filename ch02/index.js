8a6c7eaedf7d976aa41f8755a4be406df35922fd
Gapur Yovloy <kaloycant@gmail.com>
2021-08-22 22:50:04 +0300

    ch2 end

---
+++ ch02/index.js
@@ -0,0 +1,25 @@
+// 
+
+/* 
+ * bla-bla 
+ */
+
+// literals
+{
+    12;
+    1.2; //
+    "hello world";
+    'hi';
+    true;
+    false;
+    null;
+}
+
+// Идентификаторы
+{
+    let i;
+    let my_variable;
+    let v13;
+    let _dummy;
+    let $str;
+}
